41e7247c59aeb313e8230d7694315f10a4adeef9
Sameer Khan <khansameer84233@gmail.com>
2026-04-16 18:20:43 +0530

    Implement conditional video rendering in BG component based on demo path. Add logic to prevent video playback when in demo mode, enhancing user experience and responsiveness.

--- src/components/layout/BG.jsx
+++ src/components/layout/BG.jsx
@@ -3,17 +3,28 @@ import LightRays from '../3d/LightRay'
 import { Sparkles } from '@react-three/drei'
 
 const BG = () => {
+  // Check if the path contains "demo"
+  const isDemo = typeof window !== "undefined" && window.location.pathname.includes('demo');
+
   return (
     <>
-    <div className="w-full h-screen fixed top-0 left-0 z-1 overflow-hidden">
+      <div className="w-full h-screen fixed top-0 left-0 z-1 overflow-hidden">
         <LightRays raysColor="#10B981" />
       </div>
       <div className="w-full bg-black h-screen flex items-center justify-end max-[599px]:items-start max-[599px]:pt-20 pr-20 max-[599px]:px-6 absolute top-0 left-0 z-0 overflow-hidden">
-          <div className="w-1/2 max-[599px]:w-full max-[599px]:h-1/2 h-full">
-          <video src="/bg.mp4" autoPlay loop muted className="w-full h-full object-contain"></video>
-          </div>
+        <div className="w-1/2 max-[599px]:w-full max-[599px]:h-1/2 h-full">
+          {!isDemo && (
+            <video
+              src="/bg.mp4"
+              autoPlay
+              loop
+              muted
+              className="w-full h-full object-contain"
+            ></video>
+          )}
+        </div>
       </div>
-      </>
+    </>
   )
 }
 
