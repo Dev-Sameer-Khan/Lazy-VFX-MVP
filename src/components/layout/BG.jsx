2d96b01da4be0b9e6975b6cfe53ff46067672c09
Sameer Khan <khansameer84233@gmail.com>
2026-04-16 18:21:15 +0530

    Refactor video rendering logic in BG component to maintain conditional display based on demo mode. Ensure video playback is correctly wrapped in conditional rendering for improved user experience.

--- src/components/layout/BG.jsx
+++ src/components/layout/BG.jsx
@@ -11,9 +11,9 @@ const BG = () => {
       <div className="w-full h-screen fixed top-0 left-0 z-1 overflow-hidden">
         <LightRays raysColor="#10B981" />
       </div>
+          {!isDemo && (
       <div className="w-full bg-black h-screen flex items-center justify-end max-[599px]:items-start max-[599px]:pt-20 pr-20 max-[599px]:px-6 absolute top-0 left-0 z-0 overflow-hidden">
         <div className="w-1/2 max-[599px]:w-full max-[599px]:h-1/2 h-full">
-          {!isDemo && (
             <video
               src="/bg.mp4"
               autoPlay
@@ -21,9 +21,9 @@ const BG = () => {
               muted
               className="w-full h-full object-contain"
             ></video>
-          )}
         </div>
       </div>
+          )}
     </>
   )
 }
